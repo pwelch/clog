50718916808cf1f96ad13c2657f5a307f7dd874e
Paul Welch <paul@pwelch.net>
2013-12-30 20:13:03 -0500

    add to event message

--- clog.go
+++ clog.go
@@ -4,9 +4,10 @@ import (
   "fmt"
   "flag"
   "net/http"
-  "log"
   "encoding/json"
   "bytes"
+  "os"
+  "os/user"
 )
 
 // Event JSON Message
@@ -14,22 +15,43 @@ import (
 type Message struct {
   // TO DO what the hell does ` do?
   Event string `json:"entry"`
+  User string `json:"user"`
+  Hostname string `json:"hostname"`
 }
 
 type Entry struct {
   Message *Message `json:"event"`
 }
 
-// Parsing CLI arguments. note, that variables are pointers
-var strMessage = flag.String("message", "", "Message to log" )
-func init() {
-  // example with short version for long flag
-  flag.StringVar(strMessage, "m", "", "Message to log")
-}
-
 // Main
 func main() {
-  message := &Message{Event: "clog-alpha"}
+  // Parse command line flags
+  messagePtr := flag.String("m", "", "Message to log")
+  flag.Parse()
+
+  if *messagePtr == "" {
+    fmt.Println("Message can not be blank")
+    os.Exit(1)
+  }
+
+  // Get Current Username
+  usr, err := user.Current()
+  if err != nil {
+    fmt.Printf("Error: %v\n", err)
+  }
+
+  // Get Hostname
+  hostname, err := os.Hostname()
+  if err != nil {
+   fmt.Printf("Error: %v\n", err)
+   return
+  }
+
+  message := &Message{
+    Event: *messagePtr,
+    User: usr.Username,
+    Hostname: hostname,
+  }
 
   entry := &Entry{
     Message: message,
@@ -38,24 +60,23 @@ func main() {
   // create JSON
   event_entry, err := json.Marshal(entry)
   if err != nil {
-    log.Fatal(err)
+    fmt.Printf("Error: %v\n", err)
   }
  
   // print JSON for debuging
   fmt.Println("JSON:", string(event_entry))
-  flag.Parse()
 
-  // HTTP request
-  client := &http.Client{}
-  req, err := http.NewRequest("POST", "http://localhost:3000/api", bytes.NewReader(event_entry))
-  
-  // Add HTTP Headers
-  req.Header.Add("Authorization", "Token token=d298a89be686d366a72a78d92e3e43e8")
-  req.Header.Add("Content-Type", "application/json")
-  req.Header.Add("Accept", "application/json")
+ // HTTP request
+ client := &http.Client{}
+ req, err := http.NewRequest("POST", "http://localhost:3000/api", bytes.NewReader(event_entry))
+ 
+ // Add HTTP Headers
+ req.Header.Add("Authorization", "Token token=d298a89be686d366a72a78d92e3e43e8")
+ req.Header.Add("Content-Type", "application/json")
+ req.Header.Add("Accept", "application/json")
 
-  resp, err := client.Do(req)
-  defer resp.Body.Close()
+ resp, err := client.Do(req)
+ defer resp.Body.Close()
 
-  fmt.Println(resp)
+ fmt.Println(resp)
 }
